0fdb0b2c7e9fc3b93b14cf2125c79b828f9ee1d0
jak5z@o2.pl <Jajo12PK>
2019-01-17 22:15:33 +0100

    Removed unnecessary TODO

--- src/main/java/pl/jbsoft/money_transfer/RestApplication.java
+++ src/main/java/pl/jbsoft/money_transfer/RestApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-//TODO Implement CQRS for all REST API actions
-//TODO Error handling should be more flexible (now is only in prototype stage)
 @SpringBootApplication
 public class RestApplication {
 
